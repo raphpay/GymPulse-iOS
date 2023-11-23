f10e0fe7717b709f2359263b92a87e5a2ec6a411
raphpay <raphpay.dev@gmail.com>
2023-11-23 14:15:06 +0100

    first commit

---
+++ GymPulse/GymPulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymPulseApp.swift
+//  GymPulse
+//
+//  Created by Raphaël Payet on 23/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct GymPulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
