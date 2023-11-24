69ca2c7a131df734abb85bf0e6c48a5500bc0e36
raphpay <raphpay.dev@gmail.com>
2023-11-24 11:14:04 +0100

    Feature: Play workout #4

--- GymPulse/View/WorkoutView.swift
+++ GymPulse/View/WorkoutView.swift
@@ -10,16 +10,63 @@ import SwiftData
 
 struct WorkoutView: View {
     
+    @Environment(\.dismiss) var dismiss
     @Bindable var workout: Workout
+    @State private var currentExerciseIndex = 0
+    @State private var currentSeries = 1
+    
+    var currentExercise: Exercise {
+        workout.exercises[currentExerciseIndex]
+    }
+    var nextButtonText: String {
+        currentSeries < currentExercise.seriesCount ? "Next Series" : (
+            currentExerciseIndex + 1 < workout.exercises.count ? "Next Exercise" : "Finish Workout"
+        )
+    }
     
     var body: some View {
         VStack {
-            Text(workout.name)
-            Text("\(workout.breakDurationInS)")
             ForEach(workout.exercises) { exercise in
+                let isCurrentExercise = currentExercise.id == exercise.id
+                
                 Text(exercise.name)
+                    .font(isCurrentExercise ? .title : .body)
+                    .fontWeight(isCurrentExercise ? .bold : .regular)
             }
+            
+            Text("\(currentSeries) / \(currentExercise.seriesCount)")
+            
+            Button {
+                nextSeries()
+            } label: {
+                Text(nextButtonText)
+            }
+            .buttonStyle(.borderedProminent)
         }
+        .navigationTitle(workout.name)
+    }
+    
+    func nextSeries() {
+        if currentSeries < currentExercise.seriesCount {
+            currentSeries += 1
+        } else {
+            nextExercise()
+        }
+    }
+    
+    func nextExercise() {
+        if currentExerciseIndex + 1 < workout.exercises.count {
+            withAnimation {
+                self.currentExerciseIndex += 1
+                self.currentSeries = 1
+            }
+        } else {
+            finishWorkout()
+        }
+    }
+    
+    func finishWorkout() {
+        dismiss()
     }
 }
 
